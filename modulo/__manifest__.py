67386f3de075749e014d7d9f036f0be702eaa7c2
jmadrize <tecnologia@akdigitales.com>
2024-09-09 12:59:35 -0600

    add:estate_property_views

--- modulo/__manifest__.py
+++ modulo/__manifest__.py
@@ -10,7 +10,8 @@
     'application': True,
     'auto_install': False,
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml'
     ]
     
 }
