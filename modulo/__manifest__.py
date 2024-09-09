465897f3b87c5bc0c628cef2447e837376e3a2b7
jmadrize <tecnologia@akdigitales.com>
2024-09-09 09:44:19 -0600

    new: estructura modulo y manifest

---
+++ modulo/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'estate',
+    'version': '1.0',
+    'category': 'Sales/CRM',
+    'summary': 'Modulo de prueba',
+    'description': "",
+    'website': 'https://www.akdigitales.com',
+
+}
