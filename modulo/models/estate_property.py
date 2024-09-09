425cd6082e512e2ca9747d8048f119c1b873b8a4
jmadrize <tecnologia@akdigitales.com>
2024-09-09 11:19:15 -0600

    add: estate_property

---
+++ modulo/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import models
+
+class TestModel(models.Model):
+    _name = "estate_property"
+    _description = "Test Model"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Datetime()
+    expected_price = fields.Float()
+    selling_price= fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = lead_type = fields.Selection(
+        'North',
+        'South',
+        'East',
+        'West')
