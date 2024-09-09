5e1352a44eabd79ae64ce047829c4c583719e2f8
jmadrize <tecnologia@akdigitales.com>
2024-09-09 11:40:15 -0600

    fix: fields

--- modulo/models/estate_property.py
+++ modulo/models/estate_property.py
@@ -6,18 +6,3 @@ class TestModel(models.Model):
 
     name = fields.Char()
     description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Datetime()
-    expected_price = fields.Float()
-    selling_price= fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = lead_type = fields.Selection(
-        'North',
-        'South',
-        'East',
-        'West')
