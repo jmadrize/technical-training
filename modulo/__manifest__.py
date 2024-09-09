13f69fa60c59ce259582da032b551e4f83e4e773
jmadrize <tecnologia@akdigitales.com>
2024-09-09 12:41:19 -0600

    fix: manifest,

--- modulo/__manifest__.py
+++ modulo/__manifest__.py
@@ -8,7 +8,7 @@
     'website': 'https://www.akdigitales.com',
     'installable': True,
     'application': True,
-    'auto_install': False
+    'auto_install': False,
     'data': [
         'security/ir.model.access.csv'
     ]
